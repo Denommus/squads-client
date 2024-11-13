b68eb3cc52545352be802a56f642d0c54ddadc71
Yuri Albuquerque <yuridenommus@gmail.com>
2024-11-13 13:30:41 -0300

    Create transaction

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
-use std::{error::Error, sync::Arc};
+mod multisig_program;
+
+use std::{error::Error, fs::read_to_string, sync::Arc};
 
 use anchor_client::{Client, Cluster};
+use multisig_program::MultisigProgram;
+use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
 use solana_client::nonblocking::rpc_client::RpcClient;
-use solana_program::pubkey;
 use solana_sdk::{
     pubkey::Pubkey,
     signature::Keypair,
     signer::{EncodableKey, Signer},
 };
 
-const MULTISIG_PROGRAM_ID: Pubkey = pubkey!("GQGNGBWyWLQJHnnpxpNjd4qwqRK17Z3V6APS6ALee6KD");
+#[serde_as]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
+pub struct SolanaPubkey(#[serde_as(as = "DisplayFromStr")] solana_sdk::pubkey::Pubkey);
+
+impl From<SolanaPubkey> for Pubkey {
+    fn from(value: SolanaPubkey) -> Self {
+        value.0
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Config {
+    pub multisig_program_id: SolanaPubkey,
+    pub multisig: SolanaPubkey,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let payer: Arc<Keypair> = Arc::new(Keypair::read_from_file("./squads_payer.json")?);
+    let config_file = read_to_string("./config.toml")?;
+    let config: Config = toml::from_str(&config_file)?;
+
+    let instruction_payer = Arc::new(Keypair::read_from_file("./instruction_payer.json")?);
+
+    let rent_payer: Arc<Keypair> = Arc::new(Keypair::read_from_file("./squads_payer.json")?);
 
     let members: Arc<[Keypair]> = Arc::new([
         Keypair::read_from_file("./squads_member1.json")?,
@@ -25,15 +48,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let rpc_client = RpcClient::new(cluster.url().to_string());
 
-    let client = Client::new(cluster, payer.clone());
+    let client = Client::new(cluster, rent_payer.clone());
 
-    let spl_program = client.program(spl_memo::ID);
+    let spl_program = client.program(spl_memo::ID)?;
 
-    let multisig_program = client.program(MULTISIG_PROGRAM_ID);
+    let multisig_program = MultisigProgram::new(
+        &client,
+        config.multisig_program_id.into(),
+        config.multisig.into(),
+    )?;
 
     let memo_str = "Hello, World!";
 
-    let ix = spl_memo::build_memo(memo_str.as_bytes(), &[&payer.pubkey()]);
+    let ix = spl_memo::build_memo(memo_str.as_bytes(), &[&instruction_payer.pubkey()]);
+
+    let transaction_index = multisig_program.get_current_transaction_index().await? + 1;
+
+    println!("Creating transaction");
+    multisig_program
+        .create_transaction(
+            members.get(0).unwrap(),
+            rent_payer.as_ref(),
+            &[ix],
+            transaction_index,
+        )
+        .await?;
 
     println!("All good!");
 
---
+++ src/multisig_program.rs
@@ -0,0 +1,86 @@
+use std::{error::Error, sync::Arc};
+
+use anchor_client::{
+    anchor_lang::{AnchorSerialize, InstructionData},
+    Client, Program,
+};
+use solana_sdk::{
+    instruction::Instruction, message::Message, pubkey::Pubkey, signature::Keypair, signer::Signer,
+    system_program,
+};
+use squads_multisig_program::{
+    instruction::VaultTransactionCreate, Multisig, MultisigCompiledInstruction,
+    VaultTransactionCreateArgs, VaultTransactionMessage,
+};
+
+pub struct MultisigProgram {
+    program: Program<Arc<Keypair>>,
+    multisig: Pubkey,
+}
+
+impl MultisigProgram {
+    pub fn new(
+        client: &Client<Arc<Keypair>>,
+        program_id: Pubkey,
+        multisig: Pubkey,
+    ) -> Result<Self, Box<dyn Error>> {
+        let program = client.program(program_id)?;
+        Ok(MultisigProgram { program, multisig })
+    }
+
+    pub async fn get_multisig_account(&self) -> Result<Multisig, Box<dyn Error>> {
+        let acc = self.program.account(self.multisig).await?;
+        Ok(acc)
+    }
+
+    pub async fn get_current_transaction_index(&self) -> Result<u64, Box<dyn Error>> {
+        Ok(self.get_multisig_account().await?.transaction_index)
+    }
+
+    pub async fn create_transaction(
+        &self,
+        proposer: &Keypair,
+        payer: &Keypair,
+        instructions: &[Instruction],
+        transaction_index: u64,
+    ) -> Result<(), Box<dyn Error>> {
+        let (transaction_account_pda, _transaction_account_bump) = Pubkey::find_program_address(
+            &[
+                "multisig".as_bytes(),
+                self.multisig.as_ref(),
+                "transaction".as_bytes(),
+                &transaction_index.to_le_bytes(),
+            ],
+            &self.program.id(),
+        );
+        let transaction_message = Message::new(instructions, Some(&payer.pubkey()));
+
+        let create_transaction = VaultTransactionCreate {
+            args: VaultTransactionCreateArgs {
+                vault_index: 0,
+                ephemeral_signers: 0,
+                transaction_message: transaction_message.serialize().try_to_vec()?,
+                memo: None,
+            },
+        };
+
+        let accounts = squads_multisig_program::accounts::VaultTransactionCreate {
+            multisig: self.multisig,
+            transaction: transaction_account_pda,
+            system_program: system_program::ID,
+            creator: proposer.pubkey(),
+            rent_payer: payer.pubkey(),
+        };
+
+        self.program
+            .request()
+            .signer(&proposer)
+            .signer(&payer)
+            .accounts(accounts)
+            .args(create_transaction)
+            .send()
+            .await?;
+
+        Ok(())
+    }
+}
